2686feea4f9391f5eb4a5bc549b76eac2571daca
Ayomikun Oyeniyi <gideonoye1@gmail.com>
2024-12-11 11:09:46 -0600

    Add method for building trees & tests for the method

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -12,7 +12,25 @@ class Tree {
         this.root = this.buildTree(this.array);
     }
 
-    buildTree(array) {
-        
+    buildTree(array, start = 0, end = array.length - 1) {
+        if (start > end) return null;
+        const mid = Math.floor((start + end) / 2);
+
+        const root = array[mid];
+        root.left = this.buildTree(array, start, mid - 1);
+        root.right = this.buildTree(array, mid + 1, end);
+
+        return root;
     }
 }
+
+const a = new Node(1);
+const b = new Node(2);
+const c = new Node(3);
+const d = new Node(4);
+const e = new Node(5);
+const f = new Node(6);
+
+let test = [a, b, c, d, e, f];
+let tree = new Tree(test);
+console.log(tree.root);
